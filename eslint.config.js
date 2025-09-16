d10e8a98e07f8b23a9783c7b971bca14060b0f75
inogai <inogai.sign@gmail.com>
2025-09-16 16:36:46 +0800

    chore: setup eslint for tailwind

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,27 @@
 import antfu from '@antfu/eslint-config'
+import betterTailwindcss from 'eslint-plugin-better-tailwindcss'
 
-export default antfu({
-  react: true,
-}, {
-  ignores: [
-    './src/routeTree.gen.ts',
-    './src/components/ui/*.tsx',
-  ],
-})
+export default antfu(
+  {
+    react: true,
+  },
+  {
+    plugins: {
+      'better-tailwindcss': betterTailwindcss,
+    },
+    rules: {
+      ...betterTailwindcss.configs.recommended.rules,
+    },
+    settings: {
+      'better-tailwindcss': {
+        entryPoint: 'src/styles.css',
+      },
+    },
+  },
+  {
+    ignores: [
+      './src/routeTree.gen.ts',
+      './src/components/ui/*.tsx',
+    ],
+  },
+)
