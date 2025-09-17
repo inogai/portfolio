92d821a51004096633bb39ecace0ad04ca7bfeeb
inogai <inogai.sign@gmail.com>
2025-09-17 22:08:02 +0800

    ref(Header): improve

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,31 +2,35 @@ import { Link } from '@tanstack/react-router'
 
 import { Logo } from '@/components/Logo'
 
+function NavLink({ to, children }: { to: string, children: React.ReactNode }) {
+  return (
+    <Link
+      to={to}
+      className={`
+        text-muted-foreground
+        hover:text-foreground
+      `}
+    >
+      {children}
+    </Link>
+  )
+}
+
 export function Header() {
   return (
-    <header className="flex justify-between space-x-4 border-b p-4">
+    <header className={`
+      sticky top-0 flex h-(--header-height) items-center justify-between
+      space-x-4 border-b bg-background p-4
+    `}
+    >
       <Logo />
-      <Link
-        to="/"
-        className={`
-          text-muted-foreground
-          hover:text-foreground
-        `}
-      >
-        Home
-      </Link>
+
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/timeline">Timeline</NavLink>
 
       <div className="grow" />
 
-      <Link
-        to="/credits"
-        className={`
-          text-muted-foreground
-          hover:text-foreground
-        `}
-      >
-        Credits
-      </Link>
+      <NavLink to="/credits">Credits</NavLink>
     </header>
   )
 }
